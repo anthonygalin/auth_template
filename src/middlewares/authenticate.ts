c7954d8061665f17a11a71e6bf37ba6f34daf6fe
Anthony <anthonygalin@gmail.com>
2025-08-03 16:29:44 -0400

    corrected typed authenticate jwt midleware

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,10 +1,23 @@
-import { Request, Response, NextFunction } from 'express';
+import { Response, NextFunction } from 'express';
 import { verifyToken } from '../utils/jwt';
+
+enum AuthEnum {
+    bearer = 'Bearer',
+}
+
+type HeaderType = {
+    authorization: AuthEnum
+}
+
+interface Request {
+    user: User;
+    headers: HeaderType;
+}
 
 export function authenticateJWT(req: Request, res: Response, next: NextFunction) {
     const authHeader = req.headers.authorization;
 
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    if (!authHeader || !authHeader.startsWith(AuthEnum.bearer)) {
         return res.status(401).json({ error: 'No token provided' });
     }
 
